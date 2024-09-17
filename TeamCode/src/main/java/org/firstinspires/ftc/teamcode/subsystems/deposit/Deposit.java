c94f14c73da0c7215b82e71514d4002694ff32b0
mura2432 <111770840+mura2432@users.noreply.github.com>
2024-09-16 23:54:03 -0700

    Created Deposit States and considered needed static variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
@@ -1,5 +1,43 @@
 package org.firstinspires.ftc.teamcode.subsystems.deposit;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.sensors.Sensors;
+import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
+
 public class Deposit {
-    //wubba lubba dub dub
+    public enum State{
+        IDLE, //resting position everything in retracted state
+        EXTEND_SAMPLE, //extend claw arm while raising linear rail
+        EXTEND_SPECIMEN, //extend claw arm while raising linear rail
+        START_DEPOSIT_SAMPLE, //rotate claw downwards and release
+        FINISH_DEPOSIT_SAMPLE, //rotate claw upwards
+        START_DEPOSIT_SPECIMEN, //rotate claw downwards and release
+        FINISH_DEPOSIT_SPECIMEN, //rotate claw upwards
+        RETRACT, //retract claw back while lowering linear rail
+    };
+    public State state;
+
+    private HardwareQueue hardwareQueue;
+
+    public Robot robot;
+    public Slides slides;
+    public Sensors sensors;
+
+    public static double extendHeight = 1.0;
+    public static double angleSampleStart = 0.25;
+    public static double angleSampleFinish = -0.25;
+    public static double angleSpecimenStart = 0.25;
+    public static double angleSpecimenSTart = -0.25;
+
+    public Deposit(HardwareQueue hardwareQueue, HardwareMap hardwareMap, Sensors sensors, Robot robot){
+        this.hardwareQueue = hardwareQueue;
+        this.sensors = sensors;
+
+        this.state = State.IDLE;
+        this.robot = robot;
+
+        slides = new Slides(robot);
+    }
 }

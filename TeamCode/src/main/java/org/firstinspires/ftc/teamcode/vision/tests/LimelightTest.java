cd08cad00a94bab3783c1a1ac47e40fd19f35485
ryanw <ryan.wang2020@gmail.com>
2024-11-03 19:22:03 -0800

    did it with control hub, limelight works now, can get data. added some sample code that reads april tag data.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/tests/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/tests/LimelightTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.vision.tests;
 import android.util.Log;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -13,6 +14,8 @@ import org.firstinspires.ftc.teamcode.utils.Globals;
 import org.firstinspires.ftc.teamcode.utils.Pose2d;
 import org.firstinspires.ftc.teamcode.utils.RunMode;
 import org.firstinspires.ftc.teamcode.vision.Vision;
+
+import java.util.List;
 
 @TeleOp
 public class LimelightTest extends LinearOpMode {
@@ -35,16 +38,42 @@ public class LimelightTest extends LinearOpMode {
              */
             limelight.start();
             LLResult result = limelight.getLatestResult();
-            if (result != null) {
+            /*if (result != null) {
                 if (result.isValid()) {
                     Pose3D botpose = result.getBotpose();
-                    telemetry.addData("tx", result.getTx());
-                    telemetry.addData("ty", result.getTy());
-                    telemetry.addData("Botpose", botpose.toString());
-                    Log.d("", "here...." + result.getTx());
+                    Log.d("", "result: " + result.getTx());
+                }
+                else {
+                    //Log.e("", ""+limelight.isConnected());
+                    //Log.e("", "" +limelight.getStatus());
+                    Log.e("", "result is not valid");
                 }
             }
+            else
+                Log.e("", "null");
 
+             */
+
+            // print some data for each detected target
+            if (result !=null) {
+                if (result.isValid()) {
+                    // Access fiducial results
+                    List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                    for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                        telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+                    }
+
+                    // Access color results
+                    List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
+                    for (LLResultTypes.ColorResult cr : colorResults) {
+                        telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
+                    }
+                }
+            }
+            else
+                telemetry.addData("result: ", "null");
+
+            telemetry.update();
         }
     }
 }

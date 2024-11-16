0d306e85c15519c7ca42543e2dc036292c5407ff
df4edu-ftc <df4edu+ftc@gmail.com>
2024-11-15 16:32:10 -0800

    Added an opmode for just driving.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Drive.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.utils.Globals;
+import org.firstinspires.ftc.teamcode.utils.RunMode;
+
+@TeleOp
+public class Drive extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Globals.RUNMODE = RunMode.TELEOP;
+
+        Robot robot = new Robot(hardwareMap);
+
+        telemetry.addData("State", "READY TO START");
+        telemetry.update();
+
+        while (opModeInInit()) {
+            robot.update();
+        }
+
+        while (!isStopRequested()) {
+            robot.drivetrain.drive(gamepad1);
+
+            robot.update();
+
+            telemetry.update();
+        }
+    }
+}

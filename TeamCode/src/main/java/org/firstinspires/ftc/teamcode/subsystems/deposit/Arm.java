b4d2cd8fd5fc5fcb0bd6b4f085bd4d4a66dc4180
mura2432 <houcheng.jg@gmail.com>
2024-12-07 22:29:05 -0800

    deposit updates part 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Arm.java
@@ -17,6 +17,9 @@ public class Arm {
     public final PriorityServoV2 clawGrip;
 
     private final double wheelRadius = 1.0; //TODO: Get value
+    private final double smallGearNum = 20.0, smallGearRad = 1.0, bigGearNum = 40.0, bigGearRad = 1.0;
+    public final double armLength = 1.0; //TODO: Get value
+    private double currentHoriPos = 0.0;
 
     public Arm(Robot robot){
         horizontalRail = new PriorityServoV2(
@@ -24,7 +27,7 @@ public class Arm {
                 "horizontalRail",
                 PriorityServoV2.ServoType.SPEED,
                 1.0,
-                new double[] {1.0},
+                new boolean[] {false},
                 0.0,
                 1.0,
                 0.0,
@@ -39,14 +42,14 @@ public class Arm {
                 "armRotation",
                 PriorityServoV2.ServoType.SPEED,
                 1.0,
-                new double[] {1.0, -1.0},
+                new boolean[] {false, true}, //TODO: Check these - James
                 0.0,
                 1.0,
                 0.0,
                 0.0,
                 1.0,
                 3.0,
-                5.0,
+                5.0
         );
 
         clawRotation = new PriorityServoV2(
@@ -54,7 +57,7 @@ public class Arm {
                 "clawRotation",
                 PriorityServoV2.ServoType.SPEED,
                 1.0,
-                new double[] {1.0},
+                new boolean[] {false},
                 0.0,
                 1.0,
                 0.0,
@@ -69,7 +72,7 @@ public class Arm {
                 "clawGrip",
                 PriorityServoV2.ServoType.SPEED,
                 1.0,
-                new double[] {1.0},
+                new boolean[] {false},
                 0.0,
                 1.0,
                 0.0,
@@ -82,7 +85,24 @@ public class Arm {
         this.sensors = robot.sensors;
     }
 
-    public void setHorizontalPos(double target){
-        horizontalRail.setTargetAngle()
+    public void setHorizontalPos(double targetRad){
+        double wheelRot = targetRad/wheelRadius;
+        double bigGearsTraversed = wheelRot * smallGearNum * 0.5;
+        double bigGearRot = bigGearsTraversed / bigGearNum;
+
+        horizontalRail.setTargetAngle(bigGearRot);
+    }
+
+    public double getHorizontalPos(){
+        double currRad = horizontalRail.getCurrentAngle();
+        double smallGearsTraversed = currRad/(Math.PI * 2) * bigGearNum * 2;
+        double wheelRot = smallGearsTraversed/smallGearNum;
+
+        return wheelRot * 2 * Math.PI * wheelRadius;
+    }
+
+    public boolean inPosition(){
+        //TODO: Figure out what actually is essential
+        return horizontalRail.inPosition() && armRotation.inPosition() && clawRotation.inPosition() && clawGrip.inPosition();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
@@ -10,28 +10,10 @@ public class Deposit {
     public enum State{
         IDLE,
         TRANSFER_PREPARE_1,
-        TRANSFER_PREPARE_2,
+        TRANSFER_PREAPRE_2,
         TRANSFER_WAIT,
         TRANSFER_GRAB_1,
-        TRANSFER_GRAB_2,
-        TRANSFER_GRAB_3,
-        TRANSFER_END,
-        READY,
-        SAMPLE_RAISE,
-        SAMPLE_WAIT,
-        SAMPLE_DEPOSIT,
-        OUTTAKE_START,
-        OUTTAKE_DROP,
-        OUTTAKE_WAIT,
-        SPECIMEN_GRAB_START,
-        SPECIMEN_GRAB_WAIT,
-        SPECIMEN_GRAB_CLOSE,
-        SPECIMEN_GRAB_RETURN,
-        SPECIMEN_RAISE,
-        SPECIMEN_WAIT,
-        SPECIMEN_DEPOSIT,
-        RELEASE,
-        RETRACT
+        TRANSFER_GRAB_2
     };
     public State state;
 
@@ -44,7 +26,6 @@ public class Deposit {
     private double targetX, targetY;
     private double moveToX, moveToY, moveToArmAngle;
 
-    private final double initX = 0.0, initY = 0.0, initArmAngle = Math.toRadians(60.0);
     private final double intakeX = 10.0, intakeY = -2.0, intakeWaitY = -1.0;
     private final double outtakeX = -11.816, outtakeY = 0.0, grabX = -5.0, grabY = -4.0;
     private final double sampleBasketX = -2.0, sampleBasketY = 46.0;
@@ -61,265 +42,75 @@ public class Deposit {
 
     public void update(){
         if (Globals.TESTING_DISABLE_CONTROL && Globals.RUNMODE == RunMode.TESTER) return;
-        arm.update();
-
-        switch(state){
-            case IDLE:
-                break;
-            case TRANSFER_PREPARE_1:
-                setDepositPositions(intakeX, intakeWaitY);
-                calculateMoveTo();
-
-                arm.setDiffy(Math.toRadians(270.0), Math.toRadians(0.0));
-                arm.openClaw();
-
-                if(arm.checkReady()){
-                    state = State.TRANSFER_PREPARE_2;
-                }
-                break;
-            case TRANSFER_PREPARE_2:
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
 
 
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.TRANSFER_WAIT;
-                }
-                break;
-            case TRANSFER_WAIT:
-                break;
-            case TRANSFER_GRAB_1:
-                //robot.intake.transfer();
-
-                if(robot.sensors.getIntakeColor() == Sensors.BlockColor.BLUE || robot.sensors.getIntakeColor() == Sensors.BlockColor.YELLOW){
-                    state = State.TRANSFER_GRAB_2;
-                }
-                break;
-            case TRANSFER_GRAB_2:
-                setDepositPositions(intakeX, intakeY);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.TRANSFER_GRAB_3;
-                }
-                break;
-            case TRANSFER_GRAB_3:
-                arm.closeClaw();
-
-                //TODO: How would I determine when I have actually grabbed it? I used the color sensor to determine when the robot sees the sample to start intake from there, but I'm not sure how to use it to determine grabbing
-                if(arm.checkReady() && (robot.sensors.getIntakeColor() == Sensors.BlockColor.BLUE || robot.sensors.getIntakeColor() == Sensors.BlockColor.YELLOW)){
-                    state = State.TRANSFER_END;
-                }
-                break;
-            case TRANSFER_END:
-                resetToStart();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.READY;
-                }
-                break;
-            case READY:
-                break;
-            case SAMPLE_RAISE:
-                setDepositPositions(sampleBasketX, sampleBasketY);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
-                arm.setDiffy(Math.toRadians(270.0), Math.toRadians(0.0));
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.SAMPLE_WAIT;
-                }
-                break;
-            case SAMPLE_WAIT:
-                break;
-            case SAMPLE_DEPOSIT:
-                arm.openClaw();
-
-                if(arm.checkReady()){
-                    state = State.RETRACT;
-                }
-                break;
-            case OUTTAKE_START:
-                setDepositPositions(outtakeX, outtakeY);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.OUTTAKE_DROP;
-                }
-                break;
-            case OUTTAKE_DROP:
-                arm.openClaw();
-
-                if(arm.checkReady()){
-                    state = State.OUTTAKE_WAIT;
-                }
-                break;
-            case OUTTAKE_WAIT:
-                break;
-            case SPECIMEN_GRAB_START:
-                setDepositPositions(grabX, grabY);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setArmAngle(moveToArmAngle);
-                arm.setDiffy(Math.toRadians(180.0), Math.toRadians(90.0));
-                arm.openClaw();
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.SPECIMEN_GRAB_WAIT;
-                }
-                break;
-            case SPECIMEN_GRAB_WAIT:
-                break;
-            case SPECIMEN_GRAB_CLOSE:
-                arm.closeClaw();
-
-                if(arm.checkReady()){
-                    state = State.SPECIMEN_GRAB_RETURN;
-                }
-                break;
-            case SPECIMEN_GRAB_RETURN:
-                resetToStart();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setMgnPosition(moveToArmAngle);
-
-                if(arm.checkReady()){
-                    state = State.READY;
-                }
-                break;
-            case SPECIMEN_RAISE:
-                setDepositPositions(specimenBarX, specimenBarX);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setMgnPosition(moveToArmAngle);
-                arm.setDiffy(Math.toRadians(0.0), Math.toRadians(180.0));
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.SPECIMEN_WAIT;
-                }
-                break;
-            case SPECIMEN_WAIT:
-                break;
-            case SPECIMEN_DEPOSIT:
-                //TODO: How do we determine shove positions again? I feel like having a constant value will be a bad idea, which sensor would this fall under? Or would it be driver control? If its the latter, tell me when the teleop functions is made
-                setDepositPositions(specimenBarX, specimenBarY + 2.0);
-                calculateMoveTo();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setMgnPosition(moveToArmAngle);
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.RELEASE;
-                }
-                break;
-            case RELEASE:
-                arm.openClaw();
-
-                if(arm.checkReady()){
-                    state = State.RETRACT;
-                }
-                break;
-            case RETRACT:
-                resetToStart();
-
-                arm.setMgnPosition(moveToX);
-                slides.setTargetLength(moveToY);
-                arm.setMgnPosition(moveToArmAngle);
-
-                if(arm.checkReady() && slides.inPosition(0.5)){
-                    state = State.IDLE;
-                }
-                break;
-        }
     }
 
+    /*
     public void prepareTransfer() {
-        state = State.TRANSFER_PREPARE_1;
+
     }
 
     public void startTransfer() {
-        state = State.TRANSFER_GRAB_1;
+
     }
 
     public boolean isSampleReady() {
-        return state == State.READY;
+
     }
 
     public void startSampleDeposit() {
-        state = State.SAMPLE_RAISE;
+
     }
 
     public void finishSampleDeposit() {
-        state = State.SAMPLE_DEPOSIT;
+
     }
 
     public boolean isSampleDepositDone() {
-        return state == State.IDLE;
+
     }
 
     public void startOuttake() {
-        state = State.OUTTAKE_START;
+
     }
 
-    // The Robot FSM can call either grabSpecimen() or retract() after the RobotState.OUTTAKE
     public boolean isOuttakeDone() {
-        return state == State.OUTTAKE_WAIT && robot.sensors.getIntakeColor() == Sensors.BlockColor.NONE;
+
     }
 
     public void grabSpecimen() {
-        state = State.SPECIMEN_GRAB_START;
+
     }
 
-    // NOTE: This method will be called rapidly if the driver holds the "grab specimen" button
     public void finishSpecimenGrab() {
-        if(state == State.SPECIMEN_GRAB_WAIT && robot.sensors.getIntakeColor() == Sensors.BlockColor.BLUE) state = State.SPECIMEN_GRAB_CLOSE;
+
     }
 
     public boolean isSpecimenReady() {
-        return state == State.READY;
+
     }
 
     public void startSpecimenDeposit() {
-        state = State.SPECIMEN_RAISE;
+
     }
 
     public void finishSpecimenDeposit() {
-        state = State.SPECIMEN_DEPOSIT;
+
     }
 
     public boolean isSpecimenDepositDone() {
-        return state == State.IDLE;
+
     }
 
     public void retract() {
-        state = State.RETRACT;
+
     }
 
     public boolean isRetractDone(){
-        return state == State.IDLE;
+
     }
+    */
 
     public void setDepositPositions(double x, double y){
         targetX = x;
@@ -327,13 +118,13 @@ public class Deposit {
     }
 
     public void calculateMoveTo(){
-        double baseX = arm.calcMgnPosition();
+        double baseX = arm.getHorizontalPos();
         double baseY = slides.getLength();
 
         //check if only horizontal movement is possible now to minimize slides usage
-        if(Math.abs(baseY - targetY) < arm.getArmLength() && targetX >= 0.0){
-            double pos1 = targetX - Math.sqrt(arm.getArmLength() * arm.getArmLength() - (baseY - targetY) * (baseY - targetY));
-            double pos2 = targetX + Math.sqrt(arm.getArmLength() * arm.getArmLength() - (baseY - targetY) * (baseY - targetY));
+        if(Math.abs(baseY - targetY) < arm.armLength && targetX >= 0.0){
+            double pos1 = targetX - Math.sqrt(arm.armLength * arm.armLength - (baseY - targetY) * (baseY - targetY));
+            double pos2 = targetX + Math.sqrt(arm.armLength * arm.armLength - (baseY - targetY) * (baseY - targetY));
             if(Math.abs(pos1 - baseX) < Math.abs(pos2 - baseX) && pos1 >= 0.0){
                 moveToX = pos1;
             }else{
@@ -343,9 +134,9 @@ public class Deposit {
             moveToArmAngle = Math.atan2(moveToY - targetY, moveToX - targetX);
         }else{
             double slope = (baseY - targetY)/(baseX - targetX);
-            double pos1x = Utils.minMaxClip(targetX - Math.sqrt(arm.getArmLength() * arm.getArmLength()/(slope * slope + 1)), 0.0, 11.816);
+            double pos1x = Utils.minMaxClip(targetX - Math.sqrt(arm.armLength * arm.armLength/(slope * slope + 1)), 0.0, 11.816);
             double pos1y = Utils.minMaxClip(slope * (pos1x - targetX) + targetY, 0.0, 50.0);
-            double pos2x = Utils.minMaxClip(targetX + Math.sqrt(arm.getArmLength() * arm.getArmLength()/(slope * slope + 1)), 0.0, 11.816);
+            double pos2x = Utils.minMaxClip(targetX + Math.sqrt(arm.armLength * arm.armLength/(slope * slope + 1)), 0.0, 11.816);
             double pos2y = Utils.minMaxClip(slope * (pos2x - targetX) + targetY, 0.0, 50.0);
 
             if(Math.abs(pos1y - targetY) < Math.abs(pos2y - targetY)){

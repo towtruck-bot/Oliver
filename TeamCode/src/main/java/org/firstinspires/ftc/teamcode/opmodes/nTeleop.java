975ea6e1810538d3ed06424f0336e913cf6eff70
mura2432 <houcheng.jg@gmail.com>
2025-01-16 14:18:26 -0800

    added ButtonToggles for all and updated old nTeleop stuff, now adding new commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/nTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/nTeleop.java
@@ -12,42 +12,45 @@ public class nTeleop extends LinearOpMode {
     public void runOpMode(){
         Robot robot = new Robot(hardwareMap);
 
+        // Gamepad 1
+        ButtonToggle lb_1 = new ButtonToggle();
+        ButtonToggle rb_1 = new ButtonToggle();
+        ButtonToggle lt_1 = new ButtonToggle();
+        ButtonToggle rt_1 = new ButtonToggle();
+        ButtonToggle x_1 = new ButtonToggle();
+        ButtonToggle y_1 = new ButtonToggle();
+        ButtonToggle a_1 = new ButtonToggle();
+        ButtonToggle b_1 = new ButtonToggle();
+
+        //Gamepad 2
+        ButtonToggle x_2 = new ButtonToggle();
+        ButtonToggle y_2 = new ButtonToggle();
+        ButtonToggle a_2 = new ButtonToggle();
+        ButtonToggle b_2 = new ButtonToggle();
+
         final double triggerThresh = 0.2;
         final double slidesAdjustSpeed = 0.5;
-        boolean toggledAlliance = false;
         boolean speciToggled = false;
         boolean speciMode = true;
         boolean high = true;
 
-        ButtonToggle lb_1 = new ButtonToggle();
-        ButtonToggle rb_1 = new ButtonToggle();
 
         while(opModeInInit()){
             robot.update();
         }
 
         while(!isStopRequested()){
-            if (gamepad2.x) {
-                if (!toggledAlliance) {
-                    Globals.isRed = !Globals.isRed;
-                    toggledAlliance = true;
-                }
-            } else {
-                toggledAlliance = false;
+            if (x_2.isToggled(gamepad2.x)) {
+                Globals.isRed = !Globals.isRed;
             }
 
-            if (gamepad1.x) {
-                if (!speciToggled) {
-                    speciMode = !speciMode;
-                    speciToggled = true;
-                }
-            } else {
-                speciToggled = false;
+            if (x_1.isToggled(gamepad1.x)) {
+                speciMode = !speciMode;
             }
 
-            if (gamepad1.y) {
+            if (y_1.isHeld(gamepad1.y, 5)) {
                 robot.deposit.setDepositHeight(robot.deposit.getDepositHeight() + slidesAdjustSpeed);
-            } else if (gamepad1.a) {
+            } else if (a_1.isHeld(gamepad1.a, 5)) {
                 robot.deposit.setDepositHeight(robot.deposit.getDepositHeight() - slidesAdjustSpeed);
             }
 
